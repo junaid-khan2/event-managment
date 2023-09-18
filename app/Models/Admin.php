1467770ef0e7ecf891d8ab5699a8d8bc746a7a7c
junaid-khan2 <jk9044465@gmail.com>
2023-09-19 00:19:47 +0500

    first commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasFactory , HasApiTokens, Notifiable;
+    
+    protected $guard = 'admin';
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    protected $hidden = [
+      'password', 'remember_token',
+    ];
+}
---
+++ database/migrations/2023_09_18_184801_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable(); 
+            $table->bigInteger('Service_category_id')->unsigned()->nullable(); 
+            $table->string('name');
+            $table->string('image');
+            $table->string('location');
+            $table->boolean('status')->default(1);
+            $table->boolean('avalibality_for_home')->default(1);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
